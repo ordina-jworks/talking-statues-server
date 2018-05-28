42708d23a69f9aeeef00686f5de66e7b53dd8211
Nick Geudens <nickgeudens01@gmail.com>
2018-05-28 10:39:27 +0200

    init

---
+++ src/main/java/be/ordina/talkingstatues/TalkingStatuesApplication.java
@@ -0,0 +1,12 @@
+package be.ordina.talkingstatues;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkingStatuesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkingStatuesApplication.class, args);
+	}
+}
