985038e7b147bc1ffde571a02468cd279fa4635c
Nick Geudens <nickgeudens01@gmail.com>
2018-06-18 13:09:36 +0200

    TS-21 mocking Mongodb

--- src/test/java/be/ordina/talkingstatues/TalkingStatuesApplicationTests.java
+++ src/test/java/be/ordina/talkingstatues/TalkingStatuesApplicationTests.java
@@ -1,16 +1,17 @@
 package be.ordina.talkingstatues;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ContextConfiguration(classes = {TestConfig.class})
 public class TalkingStatuesApplicationTests {
+
 
 	@Test
 	public void contextLoads() {
 	}
-
 }
