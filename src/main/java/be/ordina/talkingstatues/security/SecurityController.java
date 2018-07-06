5a2bc1f1bded97892f9aa681d0acdd30fe410805
Unknown <ken.keymolen@gmail.com>
2018-07-06 10:27:18 +0200

    App User Creation Service

--- src/main/java/be/ordina/talkingstatues/security/SecurityController.java
+++ src/main/java/be/ordina/talkingstatues/security/SecurityController.java
@@ -2,8 +2,12 @@ package be.ordina.talkingstatues.security;
 
 import be.ordina.talkingstatues.appusers.AppUser;
 import be.ordina.talkingstatues.appusers.AuthService;
+import com.mongodb.util.JSON;
+import net.minidev.json.JSONArray;
+import net.minidev.json.JSONObject;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
+import java.util.LinkedHashMap;
 
 @RestController
 @RequestMapping("/auth")
@@ -28,14 +33,18 @@ public class SecurityController {
     }
 
     @GetMapping("/user")
-    public Principal user(Principal user) {
+    public Principal user(Principal user, OAuth2AuthenticationToken auth) {
+
         return user;
     }
 
     @PostMapping("/user/create")
     public void userCreation(OAuth2AuthenticationToken auth){
+        String username[];
+
         if(auth.isAuthenticated()){
-            authService.registerUser(new AppUser("", "", ""));
+            username = auth.getPrincipal().getAttributes().get("name").toString().split(" ");
+            authService.registerUser(new AppUser(auth.getPrincipal().getName(), username[0], username[1]));
         }
     }
 
