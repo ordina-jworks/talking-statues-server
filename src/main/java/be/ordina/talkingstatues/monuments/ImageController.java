7e9a91d65f7820b2524ab50b66a3330d1263bdb4
Unknown <ken.keymolen@gmail.com>
2018-08-01 14:22:33 +0200

    Put request for image upload

--- src/main/java/be/ordina/talkingstatues/monuments/ImageController.java
+++ src/main/java/be/ordina/talkingstatues/monuments/ImageController.java
@@ -3,10 +3,9 @@ package be.ordina.talkingstatues.monuments;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.InputStream;
 
 @RestController
 @RequestMapping("/images")
@@ -23,11 +22,17 @@ public class ImageController {
      * @param id
      * @return Image of a Monument
      */
+
     @GetMapping(value = "/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
     ResponseEntity getImage(@PathVariable String id){
         return ResponseEntity.ok()
                 .header(HttpHeaders.CONTENT_DISPOSITION)
                 .body(monumentService.getImageForMonumentId(id));
+    }
+
+    @PutMapping(value = "/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    void uploadImage(@RequestBody InputStream file, @PathVariable String id){
+        this.monumentService.saveImage(file, id);
     }
 
 }
